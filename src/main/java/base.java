f9fe2a5161245161a2cb8bd11b9ce5f756a8527f
Andrei Preda <apreda489@gmail.com>
2025-06-07 20:49:35 +0300

    Update base.java
    Implemented addItems method

--- src/main/java/base.java
+++ src/main/java/base.java
@@ -12,29 +12,43 @@ public class base {
 
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/");
-        Thread.sleep(3000);
-
 
         String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot", "Carrot"};
+
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+        Thread.sleep(3000);
+        addItems(driver, itemsNeeded);
+        base b = new base();
+        b.addItems(driver, itemsNeeded);
+
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.xpath("//button[normalize-space()='PROCEED TO CHECKOUT']")).click();
+        driver.findElement(By.cssSelector("input[class='promoCode']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button[class='promoBtn']")).click();
+
+    }
+
+
+    public static void addItems(WebDriver driver, String[] itemsNeeded) {
 
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
         int j = 0;
 
         for (int i= 0; i < products.size(); i++) {
-             String[] name = products.get(i).getText().split("-");
-             String formatedName = name[0].trim();
+            String[] name = products.get(i).getText().split("-");
+            String formatedName = name[0].trim();
 
-             List itemsNeededList = Arrays.asList(itemsNeeded);
+            List itemsNeededList = Arrays.asList(itemsNeeded);
 
-             if (itemsNeededList.contains(formatedName)) {
-                 j++;
-                 //Add to cart
-                 driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
-                 if (j == itemsNeeded.length) {
-                     break;
-                 }
-             }
+            if (itemsNeededList.contains(formatedName)) {
+                j++;
+                //Add to cart
+                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+                if (j == itemsNeeded.length) {
+                    break;
+                }
+            }
         }
     }
+
 }
